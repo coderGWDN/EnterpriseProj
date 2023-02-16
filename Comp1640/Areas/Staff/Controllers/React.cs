33729f4621f4ba08878be0abf9c3dd0046821aaa
minhnguy3n <minhdacamst@gmail.com>
2023-02-16 12:40:29 +0700

    Display Department List to Register User

---
+++ Comp1640/Areas/Staff/Controllers/React.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comp1640.Areas.Staff.Controllers
+{
+    public class React : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
